102d8f898666cab9d0ab55b8bd5ff582cfcaa84b
Meek Msaki <meek10x@gmail.com>
2023-11-19 00:09:32 -0600

    createAction with redux (#1)

---
+++ src/features/counter-reducer.ts
@@ -0,0 +1,33 @@
+import {
+  createAction
+} from '@reduxjs/toolkit';
+
+type CounterState = { count: number };
+
+const increment = createAction('INCREMENT', (amount: number) => {
+  return {
+    payload: amount,
+  };
+});
+
+const decrement = createAction('DECREMENT', (amount: number) => {
+  return {
+    payload: amount,
+  };
+});
+
+const reset = createAction('RESET');
+
+export const reducer = (state: CounterState, action: CounterAction) => {
+  if (action.type === increment.type) {
+    return { count: state.count + action.payload };
+  }
+  if (action.type === decrement.type) {
+    return { count: state.count - action.payload };
+  }
+  if (action.type === reset.type) {
+    return { count: 0 };
+  }
+
+  return state;
+};
