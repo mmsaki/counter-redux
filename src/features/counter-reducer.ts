5dac3a569e2191497a03fd71eaa2a487170c8538
Meek Msaki <meek10x@gmail.com>
2023-11-19 00:29:21 -0600

    export actions (createAction API #1)

--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -2,26 +2,26 @@ import { createReducer, createAction } from '@reduxjs/toolkit';
 
 type CounterState = { count: number };
 
-const increment = createAction('INCREMENT', (amount: number) => {
+export const increment = createAction('INCREMENT', (amount: number) => {
   return {
     payload: amount,
   };
 });
 
-const decrement = createAction('DECREMENT', (amount: number) => {
+export const decrement = createAction('DECREMENT', (amount: number) => {
   return {
     payload: amount,
   };
 });
 
-const reset = createAction('RESET');
+export const reset = createAction('RESET');
 
 type CounterAction =
   | ReturnType<typeof increment>
   | ReturnType<typeof decrement>
   | ReturnType<typeof reset>;
 
-const counterReducer = createReducer({ count: 0 }, (builder) => {
+export const counterReducer = createReducer({ count: 0 }, (builder) => {
   builder.addCase(increment, (state, action) => {
     state.count += action.payload;
   });
